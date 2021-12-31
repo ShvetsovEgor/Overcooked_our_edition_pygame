8184c780580ae99a8f36a337fe605494a676ea6c
anyyaa <83037109+anyyaa@users.noreply.github.com>
2021-12-31 13:05:44 +0400

    возвращает текст с кнопки в main

--- button.py
+++ button.py
@@ -1,16 +1,24 @@
 import pygame
 
-ButtonGroup = pygame.sprite.Group()
+buttongroup = pygame.sprite.Group()
 
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, screen, text, y, x):
-        super().__init__(*ButtonGroup)
+    def __init__(self, screen, x, y, text):
+        super().__init__(buttongroup)
+        self.text = text
         font = pygame.font.Font(None, 100)
         string_rendered = font.render(text, 1, pygame.Color('white'))
+
         intro_rect = string_rendered.get_rect()
+        pygame.draw.rect(string_rendered, "white", intro_rect, width=5)
 
         intro_rect.y = y
         intro_rect.x = x
         self.rect = intro_rect
         screen.blit(string_rendered, intro_rect)
+
+    def update(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(event.pos):
+            return self.text
