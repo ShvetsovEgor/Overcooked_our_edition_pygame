d5a6032e1201fd217abb458eca544e7ba59a2de2
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-03 17:15:15 +0400

    выбор уровня

--- levelchoose.py
+++ levelchoose.py
@@ -1,16 +1,18 @@
 import pygame
 from load_image import load_image
+from platform import Platform
 
 
-class LevelChoose():
+class LevelChoose:
     def __init__(self, kl):
         self.kl = kl
         self.choose_the_level()
 
     def choose_the_level(self):
+        print(232)
         if self.kl == 1 or self.kl == 2:
             infoObject = pygame.display.Info()
-            size = width, height = (infoObject.current_w - 100, infoObject.current_h - 100)
+            size = width, height = (infoObject.current_w, infoObject.current_h)
             screen = pygame.display.set_mode(size)
             fon = pygame.transform.scale(load_image('fon1.png'), (width, height))
             screen.blit(fon, (0, 0))
@@ -31,13 +33,14 @@ class LevelChoose():
             w1 = 15
             for i in range(0, kl, s):
                 if lines[i] == '-----\n':
-                    print(w, w1)
-                    pygame.draw.rect(screen, (0, 255, 0),
-                                     (w1, 300, 100, 100))
+                    Platform(screen, w1, 10, str(i + 1), "red")
+                    # pygame.draw.rect(screen, (0, 255, 0),
+                    #                  (w1, 300, 100, 100))
 
                 elif lines[i] == '-\n':
+                    Platform(screen, w1, 10, str(i + 1), "green")
                     print(w, w1)
-                    pygame.draw.rect(screen, (255, 0, 0),
-                                     (w1, 300, 100, 100))
+                    # pygame.draw.rect(screen, (255, 0, 0),
+                    #                  (w1, 300, 100, 100))
 
                 w1 += 100 + w
