c34e4149f01134f0422c2630b4e982a0417670f1
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-04 15:11:08 +0400

    добавлен food

--- levelchoose.py
+++ levelchoose.py
@@ -1,46 +1,86 @@
+import sqlite3
+
 import pygame
 from load_image import load_image
-from platform import Platform
+from players import playersgroup, Player, SecondPlayer
+from level_platform import LevelPlatform, platformgroup
+allsprites = pygame.sprite.Group()
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self, width, height):
+        self.dx = 0
+        self.dy = 0
+        self.width = width
+        self.height = height
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - self.width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - self.height // 2)
 
 
 class LevelChoose:
-    def __init__(self, kl):
-        self.kl = kl
+    def __init__(self, parent, screen, kl):
+        self.parent = parent
+        self.screen = screen
+        self.kol = kl
+
+        fon = pygame.transform.scale(load_image('fon1.png'), (self.parent.width, self.parent.height))
+        self.screen.blit(fon, (0, 0))
+        con = sqlite3.connect("level_history.db")
+        cur = con.cursor()
+        result = cur.execute("Select * from history").fetchall()
+        x = 200
+        y = self.parent.height // 2
+
+        if self.kol == 1:
+            self.obj = Player(50, y - 50, allsprites)
+        else:
+            self.obj = Player(50, y - 50, allsprites)
+            SecondPlayer(50, y + 50, allsprites)
+
+        pygame.draw.rect(self.screen, "black", (0, y - 100, x, 200))
+
+        # запрос из БД!
+        print(result)
+        y -= 100
+        for el in result:
+            # рисуем мостик или дорогу
+            if el[1]:
+                LevelPlatform(self.screen, x, y, str(el[0]), "green", allsprites)
+            else:
+                LevelPlatform(self.screen, x, y, str(el[0]), "red", allsprites)
+            x += 300
         self.choose_the_level()
 
     def choose_the_level(self):
-        print(232)
-        if self.kl == 1 or self.kl == 2:
-            infoObject = pygame.display.Info()
-            size = width, height = (infoObject.current_w, infoObject.current_h)
-            screen = pygame.display.set_mode(size)
-            fon = pygame.transform.scale(load_image('fon1.png'), (width, height))
-            screen.blit(fon, (0, 0))
-            if self.kl == 1:
-                f = open('levels1.txt', encoding='utf-8')
-            else:
-                f = open('levels2.txt', encoding='utf-8')
-            lines = f.readlines()
-            print(lines)
-            kl = lines.count('-----\n') + lines.count('-\n')
-            print(kl)
-            s = 1
-            # пока храним уровни в тхт файле, разделяем их '-----\n' и '-\n'
-            # если '-----\n' - уровень не пройден, иначе - пройден.
-            # s - количество строк, которые занимает один уровень. Все уровни занимают одинаковое количество строк.
-            w = (infoObject.current_w - 100 * (kl + 1)) // kl
-            print(infoObject.current_w, w)
-            w1 = 15
-            for i in range(0, kl, s):
-                if lines[i] == '-----\n':
-                    Platform(screen, w1, 10, str(i + 1), "red")
-                    # pygame.draw.rect(screen, (0, 255, 0),
-                    #                  (w1, 300, 100, 100))
+        FPS = 30
+        running = True
+        clock = pygame.time.Clock()
+        camera = Camera(self.parent.width, self.parent.height)
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-                elif lines[i] == '-\n':
-                    Platform(screen, w1, 10, str(i + 1), "green")
-                    print(w, w1)
-                    # pygame.draw.rect(screen, (255, 0, 0),
-                    #                  (w1, 300, 100, 100))
+                if event.type == pygame.KEYDOWN:
+                    pass
 
-                w1 += 100 + w
+            self.screen.fill((0, 0, 255))
+            camera.update(self.obj)
+            for el in allsprites:
+                camera.apply(el)
+            playersgroup.update()
+            platformgroup.draw(self.screen)
+            platformgroup.update()
+            playersgroup.draw(self.screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+            pygame.event.pump()
+        pygame.quit()
