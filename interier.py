f432e7f2cb45eb1a9cb7237d29feecce063e18a5
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-08 13:02:38 +0400

    подгоняется размер клеток

--- interier.py
+++ interier.py
@@ -6,18 +6,22 @@ obstacle = pygame.sprite.Group()
 
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, cell_size):
         super(Floor, self).__init__(allsprites)
         self.image = load_image("cell.jpg")
         self.rect = self.image.get_rect()
+        self.cell_size = cell_size
+        self.image = pygame.transform.scale(self.image, (self.cell_size, self.cell_size))
         self.rect.x = x
         self.rect.y = y
 
 
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, cell_size):
         super().__init__(allsprites, obstacle)
         self.image = load_image("wall.png")
         self.rect = self.image.get_rect()
+        self.cell_size = cell_size
+        self.image = pygame.transform.scale(self.image, (self.cell_size, self.cell_size))
         self.rect.x = x
         self.rect.y = y
