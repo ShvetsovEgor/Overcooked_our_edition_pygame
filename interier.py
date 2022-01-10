520ff78c740ee96acdc3b949dfeeeb0c2efa50ed
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-10 17:27:34 +0400

    Update interier.py

--- interier.py
+++ interier.py
@@ -1,27 +1,94 @@
 import pygame
 from load_image import load_image
 
-allsprites = pygame.sprite.Group()
-obstacle = pygame.sprite.Group()
-
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self, x, y, cell_size):
+    def __init__(self, x, y, allsprites, cell_size=50):
         super(Floor, self).__init__(allsprites)
-        self.image = load_image("cell.jpg")
+        self.image = pygame.transform.scale(load_image("floor.jpg"), (cell_size, cell_size))
+
         self.rect = self.image.get_rect()
-        self.cell_size = cell_size
-        self.image = pygame.transform.scale(self.image, (self.cell_size, self.cell_size))
         self.rect.x = x
         self.rect.y = y
 
 
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, x, y, cell_size):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
         super().__init__(allsprites, obstacle)
-        self.image = load_image("wall.png")
+        self.image = pygame.transform.scale(load_image("wall.jpg"), (cell_size, cell_size))
         self.rect = self.image.get_rect()
-        self.cell_size = cell_size
-        self.image = pygame.transform.scale(self.image, (self.cell_size, self.cell_size))
         self.rect.x = x
         self.rect.y = y
+
+
+class Fridge(pygame.sprite.Sprite):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
+        super().__init__(allsprites, obstacle)
+        self.image = load_image("fridge.png")
+        width, height = self.image.get_rect().size
+        k = width / cell_size
+        self.image = pygame.transform.scale(self.image, (cell_size, int(height / k)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - int(height / k) + cell_size
+
+
+class Oven(pygame.sprite.Sprite):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
+        super().__init__(allsprites, obstacle)
+        self.image = load_image("oven.png")
+        width, height = self.image.get_rect().size
+        k = width / cell_size
+        self.image = pygame.transform.scale(self.image, (cell_size, int(height / k)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - int(height / k) + cell_size
+
+
+class Knife(pygame.sprite.Sprite):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
+        super().__init__(allsprites, obstacle)
+        self.image = load_image("knife.png")
+        width, height = self.image.get_rect().size
+        k = width / cell_size
+        self.image = pygame.transform.scale(self.image, (cell_size, int(height / k)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - int(height / k) + cell_size
+
+
+class Sink(pygame.sprite.Sprite):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
+        super().__init__(allsprites, obstacle)
+        self.image = load_image("sink.png")
+        width, height = self.image.get_rect().size
+        k = width / cell_size
+        self.image = pygame.transform.scale(self.image, (cell_size, int(height / k)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - int(height / k) + cell_size
+
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
+        super().__init__(allsprites, obstacle)
+        self.image = load_image("box.png")
+        width, height = self.image.get_rect().size
+        k = width / cell_size
+        self.image = pygame.transform.scale(self.image, (cell_size, int(height / k)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - int(height / k) + cell_size
+        print(cell_size)
+
+
+class Table(pygame.sprite.Sprite):
+    def __init__(self, x, y, allsprites, obstacle, cell_size=50):
+        super().__init__(allsprites, obstacle)
+        self.image = load_image("table.png")
+        width, height = self.image.get_rect().size
+        k = width / cell_size
+        self.image = pygame.transform.scale(self.image, (cell_size, int(height / k)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y - int(height / k) + cell_size
