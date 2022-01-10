0e4f6c6e6bca17aa53b48e2ce13acf614236ff53
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-10 17:27:03 +0400

    добавлен спавн ингредиентов

--- gameplay.py
+++ gameplay.py
@@ -1,85 +1,94 @@
 import csv
-
-from interier import Floor, Wall, allsprites
-from level_platform import platformgroup
 import pygame.sprite
-from players import playersgroup
-
+from interier import Floor, Wall, Fridge, Oven, Knife, Sink, Box, Table
 from players import Player, SecondPlayer
+from food import Ingridients
+import sqlite3
 
 
-class GamePlayScene():
+class GamePlayScene:
     def __init__(self, parent, filename, screen):
+        self.obstacle = pygame.sprite.Group()
+        self.playersgroup = pygame.sprite.Group()
+        self.allsprites = pygame.sprite.Group()
+        self.foodgroup = pygame.sprite.Group()
         self.parent = parent
+        self.width = self.parent.width
+        self.height = self.parent.height
         self.screen = screen
         self.filename = "levels/" + filename
-        for el in allsprites:
-            el.kill()
-        for el in platformgroup:
-            el.kill()
-        for el in playersgroup:
-            el.kill()
+        self.filedb = filename[:-4]
+
         self.load_level()
-        running = True
+        self.running = True
         FPS = 60
         clock = pygame.time.Clock()
-        while running:
+        while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
-            self.screen.fill("green")
-            # print(allsprites)
-            allsprites.draw(self.screen)
-            playersgroup.draw(self.screen)
-            self.first_player.update()
-            playersgroup.update()
-            clock.tick(FPS)
-            pygame.display.flip()
+                    self.running = False
+                    self.parent.running = False
+            if self.running:
+                self.screen.fill("white")
+                self.allsprites.draw(self.screen)
+                self.playersgroup.draw(self.screen)
+                self.playersgroup.update(self.obstacle)
+                clock.tick(FPS)
+                pygame.display.flip()
         pygame.quit()
 
     def load_level(self):
-        # print(playersgroup, platformgroup)
         with open(self.filename, encoding="utf8") as csvfile:
             reader = csv.reader(csvfile, delimiter=';', quotechar='"')
             self.board = list(reader)
+        num = self.board.index(['---'])
+        self.aims = self.board[num+1:]
+        print(self.aims)
+        self.board = self.board[:num]
+        self.cell_size = min(self.width // len(self.board[0]), self.height // len(self.board))
+        self.border_x = (self.width - self.cell_size * len(self.board[0])) // 2
+        self.border_y = (self.height - self.cell_size * len(self.board)) // 2
         self.generate_level()
 
     def generate_level(self):
-        x, y = 0, 0
-        for x in range(len(self.board)):
-            for y in range(len(self.board[x])):
-                if self.board[x][y] == '.':
-                    pass
-                elif self.board[x][y] == '#':
-                    pass
-                elif self.board[x][y] == '@':
-                    pass
+        for y in range(len(self.board)):
+            for x in range(len(self.board[y])):
+                vect_x, vect_y = self.border_x + x * self.cell_size, self.border_y + y * self.cell_size
+                if self.board[y][x] == '.':
+                    Floor(vect_x, vect_y, self.allsprites, self.cell_size)
+                elif self.board[y][x] == '#':
+                    Wall(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                elif self.board[y][x] == 'f':
+                    a = self.aims[-1][0].split()[1:]
+                    Fridge(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                    for elem in a:
+                        Ingridients(vect_x, vect_y, elem, self.allsprites)
+
+                elif self.board[y][x] == 'o':
+                    Oven(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                elif self.board[y][x] == 'k':
+                    Knife(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                elif self.board[y][x] == 's':
+                    Sink(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                elif self.board[y][x] == 'b':
+                    Box(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                elif self.board[y][x] == 't':
+                    Table(vect_x, vect_y, self.allsprites, self.obstacle, self.cell_size)
+                # Декодировка символов в классы
+
         self.rows = x
         self.cols = y
-        infoObject = pygame.display.Info()
-        print(min(infoObject.current_h, infoObject.current_w))
-        print(max(x, y))
-        self.cell_size = int(min(infoObject.current_h, infoObject.current_w) / max(x, y))
-        print(self.cell_size)
-        x, y = 0, 0
-        for x in range(len(self.board)):
-            for y in range(len(self.board[x])):
-                if self.board[x][y] == '.':
-                    Floor(y * self.cell_size + 300, x * self.cell_size + 100, self.cell_size)
-                elif self.board[x][y] == '#':
-                    pass
-                    Wall(y * self.cell_size + 300, x * self.cell_size + 100, self.cell_size)
-
-                elif self.board[x][y] == '@':
-                    pass
-                # Декодировка символов в классы
 
         if self.parent.kol == 2:
             x, y = 0, 0  # вопрос
-            allsprites = pygame.sprite.Group()
-            self.first_player = Player(x, y, allsprites, self.cell_size, self.board)
-            self.second_player = SecondPlayer(x, y, allsprites)
+            self.first_player = Player(x, y, self.playersgroup, self.allsprites, self.cell_size)
+            self.second_player = SecondPlayer(x, y, self.playersgroup, self.allsprites)
         elif self.parent.kol == 1:
             x, y = 100, 100  # вопрос
-            allsprites = pygame.sprite.Group()
-            self.first_player = Player(x, y, allsprites, self.cell_size, self.board)
+            self.first_player = Player(x, y, self.playersgroup, self.allsprites, self.cell_size - 5)
+
+        con = sqlite3.connect("level_history.db")
+
+        cur = con.cursor()
+        result = cur.execute(f"SELECT * FROM history WHERE title = '{self.filedb}'").fetchall()[0][-1].split()
+        print(result)
