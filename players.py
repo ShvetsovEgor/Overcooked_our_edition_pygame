ffaea4fb4f6402a18c07108a777484a6dac3af04
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-16 18:18:04 +0400

    берет и кладет продукты

--- players.py
+++ players.py
@@ -1,10 +1,5 @@
 import pygame.sprite
 from load_image import load_image
-import csv
-from food import Ingridients
-from interier import Box, Fridge
-
-allsprites = pygame.sprite.Group()
 
 
 class Player(pygame.sprite.Sprite):
@@ -20,7 +15,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+        self.direction = None
         # предмет в руках
         self.object = None
 
@@ -33,121 +28,150 @@ class Player(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
+    def find(self, foodgroup):
+
+        if self.direction == "U":
+            self.rect.y -= 20
+            sprite = pygame.sprite.spritecollide(self, foodgroup, False)
+            if sprite is not None:
+                sprite = sprite[0]
+                sprite.parent = self
+            # print("Текущий объект", sprite.title)
+            self.rect.y += 20
+            return (1, sprite)
+
+        elif self.direction == "D":
+            self.rect.y += 20
+            sprite = pygame.sprite.spritecollide(self, foodgroup, False)
+            if sprite is not None:
+                sprite = sprite[0]
+                sprite.parent = self
+            # print("Текущий объект", sprite.title)
+            self.rect.y -= 20
+            return (1, sprite)
+
+        elif self.direction == "R":
+            self.rect.x -= 20
+            sprite = pygame.sprite.spritecollide(self, foodgroup, False)
+            if sprite is not None:
+                sprite = sprite[0]
+                sprite.parent = self
+            # print("Текущий объект", sprite.title)
+            self.rect.x += 20
+            return (1, sprite)
+
+        elif self.direction == "L":
+            self.rect.x -= 20
+            sprite = pygame.sprite.spritecollide(self, foodgroup, False)
+            if sprite is not None:
+                sprite = sprite[0]
+                sprite.parent = self
+            # print("Текущий объект", sprite.title)
+            self.rect.x += 20
+            return (1, sprite)
+
+    def put(self, obstacle, sprite_now):
+
+        if self.direction == "U":
+            self.rect.y -= 20
+            sprite = pygame.sprite.spritecollide(self, obstacle, False)
+            # print(sprite)
+            if sprite is not None and 'Floor' not in str(sprite) and "Wall" not in str(sprite):
+                sprite = sprite[0]
+                sprite_now.parent = sprite
+            # print("Текущий объект", sprite)
+            self.rect.y += 20
+            return 1
+
+        elif self.direction == "D":
+            self.rect.y += 20
+            sprite = pygame.sprite.spritecollide(self, obstacle, False)
+            if sprite is not None and 'Floor' not in str(sprite) and "Wall" not in str(sprite):
+                sprite = sprite[0]
+                sprite_now.parent = sprite
+            # print("Текущий объект", sprite)
+            self.rect.y -= 20
+            return 1
+
+        elif self.direction == "R":
+            self.rect.x -= 20
+            sprite = pygame.sprite.spritecollide(self, obstacle, False)
+            if sprite is not None and 'Floor' not in str(sprite) and "Wall" not in str(sprite):
+                sprite = sprite[0]
+                sprite_now.parent = sprite
+            # print("Текущий объект", sprite)
+            self.rect.x += 20
+            return 1
+
+        elif self.direction == "L":
+            self.rect.x -= 20
+            sprite = pygame.sprite.spritecollide(self, obstacle, False)
+            if sprite is not None and 'Floor' not in str(sprite) and "Wall" not in str(sprite):
+                sprite = sprite[0]
+                sprite_now.parent = sprite
+
+            # print("Текущий объект", sprite)
+            self.rect.x += 20
+            return 1
+
     def update(self, obstacle, foodgroup=None, plategroup=None, event=None):
-        direction = 0
         if event is not None:
             sprite = pygame.sprite.spritecollideany(self, foodgroup)
             if event.button == pygame.K_m and sprite:
                 self.object = sprite
                 sprite.parent = self
         else:
-            s = ''
             go = False
             x = self.rect.x
             y = self.rect.y
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT]:
-                direction = 'left'
                 self.rect.x -= 5
+                self.direction = "L"
                 if pygame.sprite.spritecollide(self, obstacle, False):
-                    s = str(pygame.sprite.spritecollide(self, obstacle, False)).split()[0][2:]
-                    if s == 'Box' and keys[pygame.K_SPACE]:
-                        Box.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                    if s == 'Fridge' and keys[pygame.K_SPACE]:
-                        Fridge.update(pygame.sprite.spritecollideany(self, obstacle, None))
                     self.rect.x += 5
                 else:
-
                     go = True
 
             if keys[pygame.K_RIGHT]:
-                direction = 'right'
-
                 self.rect.x += 5
+                self.direction = "R"
                 if pygame.sprite.spritecollide(self, obstacle, False):
-                    s = str(pygame.sprite.spritecollide(self, obstacle, False)).split()[0][2:]
-                    if s == 'Box' and keys[pygame.K_SPACE]:
-                        Box.update(pygame.sprite.spritecollideany(self, obstacle, None))
 
-                    if s == 'Fridge' and keys[pygame.K_SPACE]:
-                        Fridge.update(pygame.sprite.spritecollideany(self, obstacle, None))
                     self.rect.x -= 5
                 else:
+
                     go = True
             if keys[pygame.K_UP]:
-                direction = 'up'
                 self.rect.y -= 5
+                self.direction = "U"
                 if pygame.sprite.spritecollide(self, obstacle, False):
-                    s = str(pygame.sprite.spritecollide(self, obstacle, False)).split()[0][2:]
-                    if s == 'Box' and keys[pygame.K_SPACE]:
-                        Box.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                    if s == 'Fridge' and keys[pygame.K_SPACE]:
-                        Fridge.update(pygame.sprite.spritecollideany(self, obstacle, None))
                     self.rect.y += 5
                 else:
-
                     go = True
 
             if keys[pygame.K_DOWN]:
-                direction = 'down'
                 self.rect.y += 5
+                self.direction = "D"
                 if pygame.sprite.spritecollide(self, obstacle, False):
-                    s = str(pygame.sprite.spritecollide(self, obstacle, False)).split()[0][2:]
-                    if s == 'Box' and keys[pygame.K_SPACE]:
-                        Box.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                    if s == 'Fridge' and keys[pygame.K_SPACE]:
-                        Fridge.update(pygame.sprite.spritecollideany(self, obstacle, None))
                     self.rect.y -= 5
                 else:
-
                     go = True
 
             if go:
                 self.cur_frame = (self.cur_frame + 1) % len(self.frames)
                 self.image = self.frames[self.cur_frame]
 
-            '''
-            else:
-                if direction == 'up':
 
-                    self.rect.y -= 5
-                    if pygame.sprite.spritecollide(self, obstacle, False):
-
-                        s = str(pygame.sprite.spritecollide(self, obstacle, False)).split()[0][2:]
-                        print(s)
-                        if s == 'Box' and keys[pygame.K_SPACE]:
-                            Box.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                        if s == 'Fridge' and keys[pygame.K_SPACE]:
-                            Fridge.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                        self.rect.y += 5
-           
-            self.direct(direction, obstacle)
-
-    def direct(self, direction, obstacle):
-        if direction == 'up':
-
-            self.rect.y -= 5
-            if pygame.sprite.spritecollide(self, obstacle, False):
-
-                s = str(pygame.sprite.spritecollide(self, obstacle, False)).split()[0][2:]
-                print(s)
-                if s == 'Box' and keys[pygame.K_SPACE]:
-                    Box.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                if s == 'Fridge' and keys[pygame.K_SPACE]:
-                    Fridge.update(pygame.sprite.spritecollideany(self, obstacle, None))
-
-                self.rect.y += 5
-
-     '''
 class SecondPlayer(pygame.sprite.Sprite):
     def __init__(self, x, y, playersgroup, allsprites, cell_size=50):
         super().__init__(allsprites, playersgroup)
+        # self.frames = []
+        # self.cut_sheet(load_image("walk.png"), 4, 1)
+        # self.cur_frame = 0
+        # for i in range(len(self.frames)):
+        #     self.frames[i] = pygame.transform.scale(self.frames[i], (cell_size, cell_size))
+        # self.image = self.frames[self.cur_frame]
         self.image = load_image("green.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -195,3 +219,7 @@ class SecondPlayer(pygame.sprite.Sprite):
                 self.rect.y -= 5
             else:
                 go = True
+
+        # if go:
+        #     self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        #     self.image = self.frames[self.cur_frame]
