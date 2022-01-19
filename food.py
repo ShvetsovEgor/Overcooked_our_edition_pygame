b52153b65abcd465e2cd9b60409b8d476101ef70
anyyaa <83037109+anyyaa@users.noreply.github.com>
2022-01-19 23:34:09 +0400

    смена картинки

--- food.py
+++ food.py
@@ -4,8 +4,8 @@ from load_image import load_image
 
 visual_food = {"Название": ["Нормальное состояние", "Нарезанное состояние", "Сваренное состояние", "Жареное состояние"],
                "Мясо": ["meat.png"],
-               'Томат': ['tomato.png', 'sliced_tomato'],
-               "Огурец": ['cucumber.png', 'sliced_cucumber'],
+               'Томат': ['tomato.png', 'sliced_tomato.png'],
+               "Огурец": ['cucumber.png', 'sliced_cucumber.png'],
                "Картофель": ['tomato.png', 'sliced_tomato.png', 'fried_potato.png'],
                "Морковь": ['carrot.png', 'sliced_carrot.png'],
                'Салат': ['salad.png']}
@@ -17,8 +17,10 @@ visual_food = {"Название": ["Нормальное состояние", "
 class Food(pygame.sprite.Sprite):
     def __init__(self, title, parent, allsprites, foodgroup, sliced=False, boiled=False, fried=False, shaked=False):
         super().__init__(allsprites, foodgroup)
-
-        self.image = load_image(visual_food[title][0])  # загружаем фото которое соответсвует названию еды
+        if sliced:
+            self.image = load_image(visual_food[title][1])
+        else:
+            self.image = load_image(visual_food[title][0])  # загружаем фото которое соответсвует названию еды
         self.parent = parent  # запоминаем к какому объекту еда прикреплена
         x, y, width, height = self.image.get_rect()
         self.rect = self.image.get_rect()
@@ -45,6 +47,14 @@ class Food(pygame.sprite.Sprite):
     def update(self):
         self.rect.x = self.parent.rect.x
         self.rect.y = self.parent.rect.y
+
+    def change_pic(self):
+        if self.sliced:
+            self.image = load_image(visual_food[self.title][1])
+            x, y, width, height = self.image.get_rect()
+            self.rect = self.image.get_rect()
+            self.rect.x = self.parent.rect.x
+            self.rect.y = self.parent.rect.y
 
 
 plategroup = pygame.sprite.Group()
